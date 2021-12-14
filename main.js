9b7bfb7d8a1963937e708ed78c73c1f65c3a972c
MelakuAlehegn <49185991+MelakuAlehegn@users.noreply.github.com>
2021-12-14 23:17:58 +0300

    navbar fixed finished

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const icon = document.querySelector('.fas')
 const cv_btn = document.querySelector('.btn-cv')
 // console.log(icon)
 // icon.addEventListener('click', switcher())
-console.log(document)
 icon.onclick = function () {
     document.body.classList.toggle('dark-theme')
     if (document.body.classList.contains('dark-theme')) {
@@ -16,3 +15,17 @@ icon.onclick = function () {
 cv_btn.onclick = function () {
     window.open('images/MelakuAlehegnResume_4.pdf')
 }
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('fixed-top')
+            navbar_height = this.document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px'
+        }
+        else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            document.body.style.paddingTop = '0'
+
+        }
+    })
+})
