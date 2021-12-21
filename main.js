24cbca6d295f60564642504b89fcb5218c58e29a
MelakuAlehegn <49185991+MelakuAlehegn@users.noreply.github.com>
2021-12-21 19:17:02 +0300

    contact page finished/responsive

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const icon = document.querySelector('.fas')
-const cv_btn = document.querySelector('.btn-cv')
-// console.log(icon)
-// icon.addEventListener('click', switcher())
+const cv_btn = document.querySelector('.btn')
+const nav = document.querySelector('#navbar-example2')
+const li = document.querySelectorAll('.nav-link')
+
 icon.onclick = function () {
     document.body.classList.toggle('dark-theme')
     if (document.body.classList.contains('dark-theme')) {
@@ -9,23 +10,12 @@ icon.onclick = function () {
         icon.classList.add('fa-moon')
     }
     else {
+        nav.classList.add('bg-dark')
+        nav.classList.add('navbar-dark')
         icon.classList.add('fa-sun')
+
     }
 }
 cv_btn.onclick = function () {
     window.open('images/MelakuAlehegnResume_4.pdf')
 }
-document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) {
-            document.getElementById('navbar_top').classList.add('fixed-top')
-            navbar_height = this.document.querySelector('.navbar').offsetHeight;
-            document.body.style.paddingTop = navbar_height + 'px'
-        }
-        else {
-            document.getElementById('navbar_top').classList.remove('fixed-top');
-            document.body.style.paddingTop = '0'
-
-        }
-    })
-})
